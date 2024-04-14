22d5d9e02a329103cf4a6bef826220f8e0b2b655
thesureshvadde <vaddesureshvarma@gmail.com>
2024-04-14 13:35:18 +0530

    infra

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.42.0"
+      version = "5.45.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  # you can give access key and secret key here, but security problem.
   region = "us-east-1"
 }
